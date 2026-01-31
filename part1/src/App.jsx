fc7eab0014fa34429a486368705ed1aaf7fc2951
khalilulah <alausakhalil@gmail.com>
2026-01-31 15:12:54 +0100

    finished part1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -7,10 +7,26 @@ import Count from "./Count";
 import Statistics from "./Statistics";
 
 const App = () => {
+  //states for the first task
   const [bad, setBad] = useState(0);
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [total, setTotal] = useState(0);
+
+  //states for the second task
+  const [selected, setSelected] = useState(0);
+
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+  const [vote, setVote] = useState(new Array(anecdotes.length).fill(0));
 
   const handleBad = () => {
     const updatedBad = bad + 1;
@@ -60,6 +76,20 @@ const App = () => {
     ],
   };
 
+  const handleClick = () => {
+    const currentAnecdotes = Math.floor(Math.random() * anecdotes.length);
+    setSelected(currentAnecdotes);
+  };
+
+  const mostVotedIndex = vote.indexOf(Math.max(...vote));
+
+  const handleVote = () => {
+    const voteCopy = [...vote];
+    voteCopy[selected] += 1;
+
+    setVote(voteCopy);
+  };
+
   return (
     <div>
       <b>give feedback</b>
@@ -68,6 +98,8 @@ const App = () => {
         <Button onClick={handleNeutral} title="Neutral" />
         <Button onClick={handleGood} title="Good" />
       </div>
+
+      {/* STATISTICS SECTION */}
       <p>statistics</p>
       {total > 0 ? (
         <Statistics
@@ -81,6 +113,12 @@ const App = () => {
       ) : (
         <p>no feedback yet</p>
       )}
+      {/* ANECDOTES */}
+      <p>{anecdotes[selected]}</p>
+      <p>has {vote[selected]} votes</p>
+      <button onClick={handleClick}>next anecdotes</button>
+      <button onClick={handleVote}>vote</button>
+      <p>highestVote {anecdotes[mostVotedIndex]} votes</p>
     </div>
   );
 };

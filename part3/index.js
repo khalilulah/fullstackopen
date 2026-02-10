614779e5a071fb4355fa3eccd47a2208c6fe5604
khalilulah <alausakhalil@gmail.com>
2026-02-10 17:05:01 +0100

    finished exercise 3a

---
+++ part3/index.js
@@ -0,0 +1,99 @@
+const express = require("express");
+var morgan = require("morgan");
+const app = express();
+app.use(express.json());
+morgan.token("body", (req) => JSON.stringify(req.body));
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+);
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/info", (request, response) => {
+  const date = new Date().toString();
+  response.send(`<p>phonebook has info for ${persons.length} people</p>
+    <p>${date}</p>`);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+//delete a person from phonebooonk
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+//get all persons in phonebook
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+//create a new phonebook
+app.post("/api/persons", (request, response) => {
+  const generateId = Math.floor(Math.random() * 1002829);
+  body = request.body;
+
+  const existingName = persons.find(
+    (person) => person.name.toLowerCase() === body.name.toLowerCase(),
+  );
+  console.log(existingName);
+
+  if (existingName) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "fill all fields",
+    });
+  }
+
+  const person = {
+    id: generateId,
+    name: body.name,
+    number: body.number,
+  };
+  persons.push(person);
+  console.log(person);
+
+  response.json(person);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
